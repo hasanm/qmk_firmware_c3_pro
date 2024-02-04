56aeb540f88e7c13f51a3404cfea6050e865ffe2
Mahmudul Hasan <cs.mahmud@gmail.com>
2024-02-04 16:35:24 -0700

    Added Mouse Buttons in main layout

--- keyboards/keychron/c3_pro/ansi/red/keymaps/french/keymap.c
+++ keyboards/keychron/c3_pro/ansi/red/keymaps/french/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
         KC_LALT,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,
-        KC_LWIN,  KC_LALT,  KC_LCTL,                                KC_SPC,                                 KC_RCTL,  MO(WIN_FN),KC_APP,    KC_RALT,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LWIN,  KC_LALT,  KC_LCTL,                                KC_SPC,                                 KC_RCTL,  MO(WIN_FN),KC_BTN1,    KC_BTN2,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_tkl_ansi(
         _______,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  BL_DOWN,  BL_UP,    KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_1,        KC_1,  BL_STEP,
